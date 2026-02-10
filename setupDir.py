dc94957013455b41d4f70ed462a4e6cbb0f838c5
bskr21 <baskara.widhi@gmail.com>
2026-02-10 15:23:41 +0700

    📁 Added Project Directories 📁

--- setupDir.py
+++ setupDir.py
@@ -15,5 +15,16 @@ folders = [
 # Create folder based on a variable folders
 for folder in folders:
     (THIS_FOLDER / folder).mkdir(parents=True, exist_ok=True)
+print('Folders successfully created')
+
+for folder in ["data/input", "data/output", "data/processed", "data/cache"]:
+    Path(f"{folder}/.gitkeep").touch(exist_ok=True)
+print('.gitkeep files created in folder data')
+
+Path("configs/config.yaml").touch(exist_ok=True)
+Path("configs/samples/cpu_offline_id_en.yaml").touch(exist_ok=True)
+print('All Directories successsfully created')
+
+
 
 
